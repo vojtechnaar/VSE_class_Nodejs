66d0b119c75237932c817bb669a4f140704c81ed
Vojtech Naar <vojta@naar.cz>
2026-03-13 21:29:50 +0100

    Homework commit

---
+++ HW2/index.mjs
@@ -0,0 +1,17 @@
+import { readFile, writeFile } from "fs/promises";
+
+try {
+
+    const instructions = await readFile("instructions.txt", "utf8");
+
+    const [sourceFile, targetFile] = instructions.trim().split("\n");
+
+    const data = await readFile(sourceFile.trim(), "utf8");
+
+    await writeFile(targetFile.trim(), data);
+
+    console.log("File copied successfully.");
+
+} catch (err) {
+    console.log("Error:", err.message);
+}
